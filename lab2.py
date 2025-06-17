3e664045ff3b48f58ba4c68e187ca1def5e77f63
Manica Thea <manicathea@Manicas-MacBook-Pro.local>
2025-06-17 11:47:46 -0700

    added lab2.py

---
+++ lab2.py
@@ -0,0 +1,22 @@
+# Lab 2 - Manica Thea 
+
+##Step 1 
+name = "Manica Thea"
+
+## Step 2 
+nickname = "Mani"
+
+## Step 3  
+age = 15 
+
+## Step 4
+used_before = False
+
+##Step 5 
+hobbies = ["playing Roblox","hiking","playing lacroosse","walking"]
+
+## Step 6 
+favorite_things = {'movies': 'white chicks', 'food': 'In n out burgers', 'animal': 'ducks', 'color': 'blue', 'sport': 'lacrosse'}
+
+
+print(favorite_things)
